258fae1d51df4c9175e465b78c8d1044d76afa70
Tammo <tammo@users.noreply.github.com>
2020-01-22 17:43:02 +0100

    feat: define crypto api for cross platform code

---
+++ src/Crypto.ts
@@ -0,0 +1,36 @@
+/**
+ * Cross platform interface to implement crypto related functions.
+ *
+ * TODO: Maybe replace 'any' with generics as Crypto<T> so that
+ * nodejs implementations can use Crypto<Buffer> and browser-utils
+ * can use Crypto<UInt8Array>.
+ */
+export interface Crypto {
+  randomBytes: (length: number) => any;
+  secp256k1: {
+    commit: (bf: any, amount: string) => any;
+    blindSum: (bfs: any[], mode: number) => any;
+    rangeProofSign: (
+      minValue: number,
+      commitment: any,
+      commitBlind: any,
+      nonce: any,
+      base10Exp: number,
+      minBits: number,
+      actualValue: string,
+    ) => any;
+  };
+  aes: {
+    encrypt: (payload: string, key: any) => Promise<string>;
+    decrypt: (payload: string, key: any) => Promise<string>;
+  };
+  ntru: {
+    keyPair: (seed: any) => Promise<any>;
+    encrypt: (payload: string, key: any) => Promise<string>;
+    decrypt: (payload: string, key: any) => Promise<string>;
+  };
+  base64: {
+    toBytes: (payload: string) => any;
+    toString: (payload: any) => string;
+  };
+}
