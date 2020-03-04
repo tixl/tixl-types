aef549731fc5f0678f6ea36daf21782a6f2083e0
Tammo <tammo@users.noreply.github.com>
2020-03-04 20:00:56 +0100

    feat: tx types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { Block } from './Block';
+import { Blockchain } from './Blockchain';
+import { Transaction } from './Transaction';
+
 export * from './Block';
 export * from './Blockchain';
 export * from './Crypto';
@@ -5,3 +9,12 @@ export * from './Transaction';
 export * from './Keys';
 export * from './Ledger';
 export * from './Storage';
+
+export type BlockTx = {
+  tx: Transaction;
+  block: Block;
+};
+export type BlockchainTx = {
+  tx: Transaction;
+  blockchain: Blockchain;
+};
